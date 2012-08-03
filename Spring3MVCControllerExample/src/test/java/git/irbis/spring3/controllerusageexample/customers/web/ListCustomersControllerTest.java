c32595852f057846ad1f3e7313e949412e8f364a
Alexsey Nadtochey <irbis@github.com>
2012-08-03 10:07:01 +0300

    License comments have been added. Author's e-mail has been changed.

--- Spring3MVCControllerExample/src/test/java/git/irbis/spring3/controllerusageexample/customers/web/ListCustomersControllerTest.java
+++ Spring3MVCControllerExample/src/test/java/git/irbis/spring3/controllerusageexample/customers/web/ListCustomersControllerTest.java
@@ -1,3 +1,17 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package git.irbis.spring3.controllerusageexample.customers.web;
 
 import git.irbis.spring3.controllerusageexample.customers.repository.Customer;
@@ -16,7 +30,7 @@ import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAda
 
 /**
  *
- * @author A.Nadtochey <irbis4lan@yandex.ua>
+ * @author Alexsey Nadtochey <A.Nadtochey@gmail.com>
  */
 public class ListCustomersControllerTest {
     
